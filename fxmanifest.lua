87fcad056ac9115c747bc198bddc961932714816
TabShi <145850720+TabShi01@users.noreply.github.com>
2023-11-02 20:58:53 +0100

    new fxmanifest.lua you have to replace the other

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+lua54 'yes'
+author 'Fly Crimelife x Development'
+description 'discord.gg/flycl'
+game 'gta5'
+
+clinet_scripts {
+    'config.lua',
+    'client/main.lua'
+}
